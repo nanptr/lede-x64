eff52fa32c4120f08d391cd0173d1c89619c6cc9
nanptr <76614722+nanptr@users.noreply.github.com>
2023-01-31 17:32:49 +0800

    Create diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,27 +17,14 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-# Clone community packages
-mkdir package/community
-pushd package/community
-
 # Add adguardhome 
-svn export https://github.com/Lienol/openwrt-package/branches/other/luci-app-adguardhome
+svn export https://github.com/Lienol/openwrt-package/branches/other/luci-app-adguardhome package/luci-app-adguardhome
 
-# Add luci-app-ssr-plus,vssr
-git clone --depth=1 https://github.com/fw876/helloworld 
-svn export https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall
-git clone --depth=1 https://github.com/jerrykuku/lua-maxminddb.git
-git clone --depth=1 https://github.com/jerrykuku/luci-app-vssr.git 
+# Add luci-app-ssr-plus
+git clone https://github.com/fw876/helloworld package/luci-app-ssr-plus
 
 # Add luci-app-smartdns
-svn export https://github.com/281677160/openwrt-package/trunk/luci-app-smartdns
+svn export https://github.com/281677160/openwrt-package/trunk/luci-app-smartdns package/luci-app-smartdns
 
 # Add luci-app-openclash
-svn export https://github.com/vernesong/OpenClash/trunk/luci-app-openclash
-
-# Add ddnsto & linkease
-svn export https://github.com/linkease/nas-packages-luci/trunk/luci/luci-app-ddnsto
-svn export https://github.com/linkease/nas-packages/trunk/network/services/ddnsto
-
-popd
+svn export https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
